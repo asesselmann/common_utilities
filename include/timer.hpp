e39deb5df9016e91d2d0e115c1af054967d2c02d
letrend <simon.trendel@tum.de>
2016-03-08 13:43:21 +0100

    removed unnecessary controllerstate id from trajectory service

--- include/timer.hpp
+++ include/timer.hpp
@@ -11,12 +11,12 @@ public:
     double stop(){
         t2 = high_resolution_clock::now();
         time_span = duration_cast<duration<double>>(t2 - t1);
-        return time_span.count();
+        return time_span.count(); // in seconds
     }
     double elapsedTime(){
         t2 = high_resolution_clock::now();
         time_span = duration_cast<duration<double>>(t2 - t1);
-        return time_span.count();
+        return time_span.count(); // in seconds
     }
 private:
     high_resolution_clock::time_point t1;

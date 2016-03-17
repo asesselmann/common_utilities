2878b991838487bb6c7d17fb35ab8ff29fa490e0
letrend <simon.trendel@tum.de>
2016-03-17 20:54:11 +0100

    added timer functions for milli and micro seconds

--- include/timer.hpp
+++ include/timer.hpp
@@ -18,6 +18,16 @@ public:
         time_span = duration_cast<duration<double>>(t2 - t1);
         return time_span.count(); // in seconds
     }
+    double elapsedTimeMilliSeconds(){
+        t2 = high_resolution_clock::now();
+        time_span = duration_cast<milliseconds>(t2 - t1);
+        return time_span.count(); // in milliseconds
+    }
+    double elapsedTimeMicroSeconds(){
+        t2 = high_resolution_clock::now();
+        time_span = duration_cast<microseconds>(t2 - t1);
+        return time_span.count(); // in microseconds
+    }
 private:
     high_resolution_clock::time_point t1;
     high_resolution_clock::time_point t2;

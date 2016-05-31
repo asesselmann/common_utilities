4f1a9b06c59dbd192a7f289cf5ffb2feea18aeb3
letrend <simon.trendel@tum.de>
2016-05-31 10:35:30 +0200

    fixed bug causing not all files to be converted

--- python/reduceMeshesInDirectory.py
+++ python/reduceMeshesInDirectory.py
@@ -10,7 +10,8 @@ print(argv)
 if len(argv)!=4:
     print('USAGE: path/to/dae/directory/ outputpath/directory/ ration[0-1]')
 else:
-    onlyfiles = [f for f in listdir(argv[1]) if isfile(join(argv[2], f))]
-    for file in onlyfiles:
+    files = [f for f in listdir(argv[1]) ]
+    print('Converting '+ str(len(files)) +' files')
+    for file in files:
         if file.endswith('.dae'):
             call(["blender", "--background", "--python", "reduceMesh.py", "--", join(argv[1],file), join(argv[2],file), argv[3] ])

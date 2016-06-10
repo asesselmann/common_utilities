3cab0a4794b1070bb2fb602a660653fcfe679d38
letrend <simon.trendel@tum.de>
2016-06-10 10:55:23 +0200

    fixed sed bug in renameAllFiles

--- bash/renameAllFilesInDirectory.bash
+++ bash/renameAllFilesInDirectory.bash
@@ -7,7 +7,7 @@ if [ "$#" -ne 3 ]; then
     echo "           ls ."
     echo "           fuckYou.bash"
 else
-    ls -1 $1|grep $2|column -c 100
+    ls -1 -R $1|grep $2|column -c 100
     echo "-------"
     echo "WARNING: the above files will be changed *$2* -> *$3*"
     echo "-------"
@@ -15,10 +15,10 @@ else
     response=${response,,}    # tolower
     if [[ $response =~ ^(yes|y)$ ]]; then
 	datetimestring=$(date +'%d%m%Y_%H-%M')
-        find $1 -type f -name '*'$2'*' | while read FILE ; do
-            newfile="$(echo ${FILE} |sed -e "s/\\$2/$3/")" ;
+        find $1 -type f -iname "*$2*" | while read FILE ; do
+            newfile="$(echo ${FILE} |sed -e "s/$2/$3/")" ;
             mv "${FILE}" "${newfile}" ;
-	    echo "${FILE} -> ${newfile}" >> renameAllFilesInDirectory_${datetimestring}.log
+	    echo "${FILE} -> ${newfile}" >> renameAllFilesInDirectory_${datetimestring}.log;
         done
     else
         echo "ABORT"
